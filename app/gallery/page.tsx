322f0f7bfe840974f3f52ee495e31bfa7975f5b4
Chris Fahey <chrisfahey1010@gmail.com>
2025-06-05 15:12:13 -0700

    Reduce font size for Gallery post titles

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -24,8 +24,8 @@ export default async function GalleryPage() {
                   />
                 </div>
                 <div className="mt-2">
-                  <h2 className="text-xl font-semibold">{post.title}</h2>
-                  <p className="text-sm text-gray-400">{post.date}</p>
+                  <h2 className="text-base font-semibold truncate">{post.title}</h2>
+                  <p className="text-sm text-gray-500">{post.date}</p>
                 </div>
               </div>
             </Link>
