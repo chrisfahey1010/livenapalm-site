daaa4cf940d4db2e3e1a04341cfa177c38f62673
Chris Fahey <chrisfahey1010@gmail.com>
2025-06-10 02:10:59 -0700

    Removed unused variable from download-album route

--- app/api/download-album/route.ts
+++ app/api/download-album/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import JSZip from 'jszip';
 
 if (!process.env.AWS_REGION) {
