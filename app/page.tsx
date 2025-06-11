1bad0abb0f031b7e49d6eda13df5f3daac7e4076
Chris Fahey <chrisfahey1010@gmail.com>
2025-06-11 02:23:28 -0700

    Add social media links to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function HomePage() {
   return (
@@ -24,6 +25,80 @@ export default function HomePage() {
             className="inline-block mt-4 px-6 py-3 border border-white text-white rounded-lg hover:bg-white hover:text-black transition"
           >
             Learn More
+          </Link>
+        </div>
+        
+        {/* Social Media Icons */}
+        <div className="mt-8 flex justify-center space-x-6">
+          <Link
+            href="https://instagram.com/livenapalm"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition"
+          >
+            <Image
+              src="/icons/instagram.png"
+              alt="Instagram"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
+          </Link>
+          <Link
+            href="https://github.com/chrisfahey1010/livenapalm-site#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition"
+          >
+            <Image
+              src="/icons/github.png"
+              alt="GitHub"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
+          </Link>
+          <Link
+            href="https://x.com/livenapalm"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition"
+          >
+            <Image
+              src="/icons/x.png"
+              alt="X (Twitter)"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
+          </Link>
+          <Link
+            href="https://flickr.com/photos/livenapalm/albums"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition"
+          >
+            <Image
+              src="/icons/flickr.png"
+              alt="Flickr"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
+          </Link>
+          <Link
+            href="https://youtube.com/@livenapalm"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:opacity-80 transition"
+          >
+            <Image
+              src="/icons/youtube.png"
+              alt="YouTube"
+              width={32}
+              height={32}
+              className="w-8 h-8"
+            />
           </Link>
         </div>
       </div>
