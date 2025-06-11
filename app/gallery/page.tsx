454750f91c1d00cbbd35ff768efcb6e0e63a6095
Chris Fahey <chrisfahey1010@gmail.com>
2025-06-11 03:18:17 -0700

    Adjust font size of Post titles and dates for mobile

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -24,8 +24,8 @@ export default async function GalleryPage() {
                   />
                 </div>
                 <div className="mt-2">
-                  <h2 className="text-base font-semibold truncate">{post.title}</h2>
-                  <p className="text-sm text-gray-500">{post.date}</p>
+                  <h2 className="text-xs md:text-sm lg:text-base font-semibold truncate">{post.title}</h2>
+                  <p className="text-xs md:text-sm text-gray-500">{post.date}</p>
                 </div>
               </div>
             </Link>
